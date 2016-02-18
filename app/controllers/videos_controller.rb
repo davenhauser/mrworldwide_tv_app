43d878af77ee891d1c6ba8787165b7c077f7f7ab
david niederhauser <niederhauser.dave@gmail.com>
2016-02-17 22:54:18 -0800

    add destroy

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,21 +8,48 @@ class VideosController < ApplicationController
     @video = Video.new
   end
 
-    def create
+  def create
 
-      @video = Video.new(self.video_params)
+    @video = Video.new(self.video_params_create)
 
-      #binding.pry
+    #binding.pry
 
-      if @video.save
-        redirect_to videos_path
+    if @video.save
+    redirect_to videos_path
 
-      else
-          render :new
+    else
+      render :new
         end
     end
 
-    def video_params
+    def show
+      @video = Video.find(params[:id])
+    end
+
+    def edit
+      @video = Video.find(params[:id])
+    end
+
+    def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(params.require(:video).permit(:embed_url, :title, :album, :featured_artists, :released_on, :tags))
+      redirect_to videos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to videos_path
+  end
+
+    def video_params_create
       params.require(:video).permit(:title, :embed_url, :released_on, :featured_artists)
     end
+
+    #def video_params_update
+     # params.permit.permit(:tags)
+    #end
 end
